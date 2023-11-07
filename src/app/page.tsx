d925c95a99544af520d19f36f6359970b59efca2
Aryan Jabbari <AryanJabbari@gmail.com>
2023-11-07 15:01:38 -0500

    feat(homepage): add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,136 @@
-import Image from 'next/image'
+import { clsx } from 'clsx';
+import Link from 'next/link';
+import type { DetailedHTMLProps, HTMLAttributes } from 'react';
+import { FaBitcoin } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center">
+      <SectionWithGutters className="flex flex-col gap-y-8 max-w-2xl">
+        <span className="text-bitcoin-orange self-center text-9xl">
+          <FaBitcoin />
+        </span>
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-center text-4xl font-bold">
+            Use Bitcoin to send money to your friends and family.
+          </h1>
+          <h2 className="text-center text-2xl font-medium">
+            Say no to Western Union and MoneyGram.
+          </h2>
         </div>
-      </div>
+      </SectionWithGutters>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <SectionWithGutters>
+        <p>Why use Bitcoin instead of Western Union or MoneyGram?</p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="grid grid-cols-1 md:grid-cols-3 max-w-4xl gap-8">
+          <StepCard className="gap-y-4">
+            <p>
+              Bitcoin is <strong>cheaper</strong>
+            </p>
+            <p>
+              Sending money through Western Union or MoneyGram can cost you a staggering $35 on a
+              mere $200 transfer – a whopping 17.5%! With Bitcoin, enjoy cost-effective transfers
+              that won&apos;t break the bank. Keep more of your hard-earned money where it belongs
+              &mdash; in your pocket.
+            </p>
+          </StepCard>
+          <StepCard className="gap-y-4">
+            <p>
+              Bitcoin is <strong>faster</strong>
+            </p>
+            <p>
+              Western Union and MoneyGram: Domestic transfers take up to a day, international ones
+              up to five business days.
+            </p>
+            <p>Bitcoin: Near-instant in 10 minutes, even instant with Lightning Network</p>
+          </StepCard>
+          <StepCard className="gap-y-4">
+            <p>
+              Bitcoin is <strong>more accessible</strong>
+            </p>
+            <p>
+              Bitcoin breaks down barriers, providing access to financial services that were once
+              out of reach. No banks, no borders, just seamless global transactions at your
+              fingertips.{' '}
+            </p>
+          </StepCard>
+        </div>
+      </SectionWithGutters>
+
+      <SectionWithGutters className="grid grid-cols-1 md:grid-cols-3 max-w-4xl gap-8">
+        <StepCard>
+          <StepNumber>1</StepNumber>
+          <p>Buy Bitcoin</p>
+          <ol className="list-decimal list-inside">
+            <li className="list-item">Find a Bitcoin exchange in your country.</li>
+            <li className="list-item">Exchange your local currency for Bitcoin.</li>
+          </ol>
+        </StepCard>
+        <StepCard>
+          <StepNumber>2</StepNumber>
+          <p>Send Bitcoin</p>
+          <ol className="list-decimal list-inside">
+            <li className="list-item">Ask the receiving party to create a Bitcoin wallet.</li>
+            <li className="list-item">Ask the receiver party for their Bitcoin address.</li>
+            <li className="list-item">
+              Withdraw from the Bitcoin exchange to the receiving party&apos;s Bitcoin address
+            </li>
+          </ol>
+        </StepCard>
+        <StepCard>
+          <div className="text-center">
+            <StepNumber>3</StepNumber>
+          </div>
+          <p>
+            Convert Bitcoin to cash <span className="text-xs">(optional)</span>
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div className="flex flex-col gap-y-4">
+            <ol className="list-decimal list-inside">
+              <li className="list-item">Find a Bitcoin exchange in the receiving country.</li>
+              <li className="list-item">Exchange Bitcoin for the receiver&apos;s currency.</li>
+            </ol>
+            <p>
+              Alternatively, use{' '}
+              <a
+                className="hover:text-blue-800 text-blue-500"
+                href="https://coinatmradar.com"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Coin ATM Radar
+              </a>{' '}
+              to withdraw Bitcoin to cash.
+            </p>
+          </div>
+        </StepCard>
+        <Link href="/learn-more">Learn More</Link>
+      </SectionWithGutters>
+    </div>
+  );
 }
+
+const StepCard: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = (
+  props
+) => (
+  <div
+    {...props}
+    className={clsx(
+      props.className,
+      'flex flex-col p-4 relative md:max-w-xs px-8 pb-8 rounded-lg shadow-xl'
+    )}
+  />
+);
+
+const StepNumber: React.FC<
+  DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>
+> = (props) => <p {...props} className={clsx(props.className, 'bg text-center')} />;
+
+const SectionWithGutters: React.FC<DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>> = (
+  props
+) => (
+  <section
+    {...props}
+    className={clsx(props.className, 'flex-grow items-center px-4 py-8 md:px-0')}
+  />
+);
